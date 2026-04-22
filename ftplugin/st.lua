59195d5d280f2f3e257e5b9f98e1fe98ea542a7f
Andreas <1990andreaslindblad@gmail.com>
2026-04-22 15:16:10 +0200

    Working Filedetection
    En enkel filedetection fungerar

--- ftplugin/st.lua
+++ ftplugin/st.lua
@@ -1,8 +1,8 @@
 -- 21/04/2026
---
+-- Konfiguration för Structured Text
 vim.opt_local.expandtab = true -- Använd mellanslag istället för tab
 vim.opt_local.shiftwidth = 10 -- Indrag på 4 mellanslag
 vim.opt_local.softtabstop = 10
-vim.opt_local.commentstring = "//" -- Kommentar-format för ST
+vim.opt_local.commentstring = "// %s" -- Kommentar-format för ST
 
 print("Laddade inställningar för Structured Text!")
--- lua/st-pro/core/ftdetect.lua
+++
@@ -1,5 +0,0 @@
-vim.filetype.add({
-  extenstion = {
-    st = "Structured Text",
-  },
-})
