b2c234867678f07a31cfe43fcd21b34039cbe02d
Andreas <1990andreaslindblad@gmail.com>
2026-04-24 20:42:55 +0200

    Update to syntax highlight

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,5 +3,9 @@ return {
   opts = {
     auto_install = false,
     ensure_installed = { "lua", "vim", "vimdoc", "json", "yaml", "markdown", "bash", "python", "c", "cpp" },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
   },
 }
