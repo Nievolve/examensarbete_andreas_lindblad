019570a91542774138ea55c894696744636d6933
Andreas <1990andreaslindblad@gmail.com>
2026-04-24 20:12:19 +0200

    Update

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- Basic config
-
+local nvim_path = vim.fn.stdpath("config")
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 -- REMAP
@@ -11,7 +11,7 @@ local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 parser_config.st = {
 
   install_info = {
-    url = "C:Users\andreOneDrive - sti.seProject\tree-sitter_ST",
+    url = nvim_path .. "/tree-sitting_ST",
     files = { "src/parser.c" },
     generate_requries_npm = false,
     requires_generate_from_grammar = false,
