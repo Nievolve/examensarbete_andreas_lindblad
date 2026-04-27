c29a557647038365191e2c2ec1ae8773510a3709
Andreas <1990andreaslindblad@gmail.com>
2026-04-27 13:26:35 +0200

    Update before highlight
    Update to comments

--- queries/st/highlights.scm
+++ queries/st/highlights.scm
@@ -3,10 +3,10 @@
 (keyword) @keyword
 ["IF" "THEN" "ELSE" "END_IF" "CASE" "OF" "END_CASE" "FOR" "TO" "BY" "DO" "END_FOR" "WHILE" "END_WHILE" "REPEAT" "UNTIL" "END_REPEAT"] @keyword
 
-; Blocks
+; Blocks = type .definition
 ["FUNCTION_BLOCK" "END_FUNCTION_BLOCK" "PROGRAM" "END_PROGRAM" "VAR" "VAR_INPUT" "VAR_OUTPUT" "VAR_IN_OUT" "END_VAR"] @type.definition
 
-; Data types
+; Data types = type
 ["BOOL" "INT" "DINT" "REAL" "TIME" "STRING" "WORD" "DWORD"] @type
 
 ; Functions
