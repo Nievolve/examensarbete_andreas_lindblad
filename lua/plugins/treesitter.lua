160f76dafab3ca28f8565720a4815b3807c9c65a
Andreas <1990andreaslindblad@gmail.com>
2026-04-28 10:13:46 +0200

    Update and push before
    Gör lite förändringar och test. Fortsatt problem med c-compiler plus
    errors.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
-    -- 1. Lägg till din lokala parser i konfigurationen
+    -- 1. Berätta för Treesitter att använda din GCC-kompilator
+    require("nvim-treesitter.install").compilers = { "gcc" }
+
+    -- 2. Registrera språket "st" manuellt
     local parsers = require("nvim-treesitter.parsers")
     parsers.get_parser_configs().st = {
       install_info = {
@@ -14,16 +17,9 @@ return {
       filetype = "st",
     }
 
-    -- 2. Uppdatera opts-tabellen som skickas till setup()
-    -- Vi lägger till "st" i listan över språk som ska vara installerade
+    -- 3. Tvinga in 'st' i listan över installerade språk
     if type(opts.ensure_installed) == "table" then
-      vim.list_extend(opts.ensure_installed, { "lua", "vim", "json", "st" })
+      table.insert(opts.ensure_installed, "st")
     end
-
-    -- 3. Se till att highlight är aktiverat
-    opts.highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    }
   end,
 }
