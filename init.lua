33ac4b0a620397c470bd54dbe84834b0b6b70991
Nievolve <1990andreaslindblad@gmail.com>
2026-05-12 20:15:21 +0200

    Working as intendet
    Merging branch into tree-sitter.branch

--- init.lua
+++ init.lua
@@ -5,23 +5,31 @@ vim.keymap.set("i", "<C-H>", "<C-W>", { noremap = true })
 
 -- Commands
 
-vim.api.nvim_create_user_command("convert", function()
-  -- Path to open file
+vim.api.nvim_create_user_command("Convert", function()
+  -- 1. Sökvägar
   local target_path = vim.fn.expand("%:p")
-
   local original_filename = vim.fn.expand("%:t")
-  local original_directory = vim.fn.expand("%p:h")
-  -- Current date
-  -- Add time
-  local date = os.date("%Y-%m-%-d")
-  -- Mosh
-  local output_filename = date .. "-" .. original_filename
-  local output_path = original_directory .. "/" .. output_filename
+  local original_directory = vim.fn.expand("%:p:h")
 
+  -- 2. "Skrubba" filändelsen: Byt ut .st mot .xml
+  -- %-tecknet används för att "escapea" punkten i Lua-patterns
+  local clean_filename = string.gsub(original_filename, "%.st$", "")
+  local final_output_name = clean_filename .. ".xml"
+
+  -- 3. Datum
+  local date = os.date("%Y-%m-%d")
+
+  -- 4. Bygg output-sökväg (Datum-Filnamn.xml)
+  local output_filename = date .. "-" .. final_output_name
+  local output_path = original_directory .. "\\" .. output_filename
+
+  -- 5. Kommandot
   local command = string.format(
     '!python3 "C:\\Users\\andre\\OneDrive - sti.se\\Project\\Python_ETL_ST_XML\\main.py" "%s" "%s"',
     target_path,
     output_path
   )
+
   vim.cmd(command)
-end, {})
+  print("Exporterad till: " .. output_filename)
+end, { desc = "Konverterar .st till .xml via Python" })
